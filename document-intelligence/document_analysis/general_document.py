86e65fc9fc3e450d947210baab2595d3a21abee1
JoonseobKim <wnsjtq781@naver.com>
2026-07-01 13:28:13 +0900

    Fix: correctly add document-intelligence folder

---
+++ document-intelligence/document_analysis/general_document.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(env_path)
+
+endpoint = os.getenv("DOCUMENT_INTELLIGENCE_ENDPOINT")
+key = os.getenv("DOCUMENT_INTELLIGENCE_KEY")
+
+
+# sample document
+formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+
+document_analysis_client = DocumentAnalysisClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+    
+poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-document", formUrl)
+result = poller.result()
+
+print("----Key-value pairs found in document----")
+for kv_pair in result.key_value_pairs:
+    if kv_pair.key and kv_pair.value:
+        print("Key '{}': Value: '{}'".format(kv_pair.key.content, kv_pair.value.content))
+    else:
+        print("Key '{}': Value:".format(kv_pair.key.content))
+
+print("----------------------------------------")
---
+++ document-intelligence/document_analysis/ocr.py
@@ -0,0 +1,83 @@
+"""
+This code sample shows Prebuilt Read operations with the Azure AI Document Intelligence client library.
+The async versions of the samples require Python 3.8 or later.
+
+To learn more, please visit the documentation - Quickstart: Document Intelligence (formerly Form Recognizer) SDKs
+https://learn.microsoft.com/azure/ai-services/document-intelligence/quickstarts/get-started-sdks-rest-api?pivots=programming-language-python
+"""
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+import numpy as np
+
+"""
+Remember to remove the key from your code when you're done, and never post it publicly. For production, use
+secure methods to store and access your credentials. For more information, see 
+https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
+"""
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(env_path)
+
+endpoint = os.getenv("DOCUMENT_INTELLIGENCE_ENDPOINT")
+key = os.getenv("DOCUMENT_INTELLIGENCE_KEY")
+
+def format_bounding_box(bounding_box):
+    if not bounding_box:
+        return "N/A"
+    reshaped_bounding_box = np.array(bounding_box).reshape(-1, 2)
+    return ", ".join(["[{}, {}]".format(x, y) for x, y in reshaped_bounding_box])
+
+def analyze_read():
+    # sample document
+    formUrl = "https://cdn.hancom.com/link/docs/%ED%95%9C%EA%B8%80%EB%AC%B8%EC%84%9C%ED%8C%8C%EC%9D%BC%ED%98%95%EC%8B%9D_5.0_revision1.3.pdf"
+
+    document_intelligence_client  = DocumentIntelligenceClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+    
+    poller = document_intelligence_client.begin_analyze_document(
+        "prebuilt-read", AnalyzeDocumentRequest(url_source=formUrl)
+    )
+    result = poller.result()
+
+    print ("Document contains content: ", result.content)
+
+    for idx, style in enumerate(result.styles):
+        print(
+            "Document contains {} content".format(
+                "handwritten" if style.is_handwritten else "no handwritten"
+            )
+        )
+
+    for page in result.pages:
+        print("----Analyzing Read from page #{}----".format(page.page_number))
+        print(
+            "Page has width: {} and height: {}, measured with unit: {}".format(
+                page.width, page.height, page.unit
+            )
+        )
+
+        for line_idx, line in enumerate(page.lines):
+            print(
+                "...Line # {} has text content '{}' within bounding box '{}'".format(
+                    line_idx,
+                    line.content,
+                    format_bounding_box(line.polygon),
+                )
+            )
+
+        for word in page.words:
+            print(
+                "...Word '{}' has a confidence of {}".format(
+                    word.content, word.confidence
+                )
+            )
+
+    print("----------------------------------------")
+
+
+if __name__ == "__main__":
+    analyze_read()
